23aefc761aa1d6f1be613202e4b499a36300dd5e
Michael Odinokov <mihhail.odinokov@gmail.com>
2023-02-02 20:39:47 +0200

    1

--- Järjendid/Järjendid.py
+++ Järjendid/Järjendid.py
@@ -1,4 +1,94 @@
-﻿#Самостоятельная работа
+﻿from random import*
+#praktiline töö
+#6
+
+
+
+#5
+lists=[['крот', 'белка', 'выхухоль'], ['a', 'aa', 'aaa', 'aaaa', 'aaaaa'], ['qweasdqweas', 'q', 'rteww', 'ewqqqqq']]
+
+def normalize_list(lst):
+    max_len = max([len(s) for s in lst])
+    return [s.ljust(max_len, '_') for s in lst]
+result=[normalize_list(l) for l in lists]
+print(result)
+
+
+
+print()
+#4
+numbers=[3, -4, 1, -2, 6, -5]
+s_numbers=sorted(numbers, key=lambda x: abs(x)) # сортировка по возрастанию абсолютного значения
+print(s_numbers)
+s_numbers=sorted(numbers, key=lambda x: abs(x), reverse=True)# сортировка по убыванию абсолютного значения
+print(s_numbers)
+
+
+print()
+#3
+arvud=[]
+kogus=int(input("Kogus: "))
+for i in range(kogus):
+    arvud.append(randint(-100,100))
+    print(arvud)
+    max_arv=max(arvud)
+    ind=arvud.index(max_arv)
+    print(ind)
+max_arv=round(max_arv/kogus,2)
+arvud.insert(ind, max_arv)
+arvud.pop(ind+1)
+print(arvud)
+print()
+    
+
+
+
+#2
+s=""
+l=["Kot", "Enot", "Belka"]
+l_list=list(l)
+
+osa1=[]
+osa2=[]
+print(l_list)
+while True:
+    try:
+        if len(l)%2==0 and len(l)>=2:
+            n=int(len(l)/2)
+            for i in range(1, n+1):
+                osa1.append(l[i])
+            for j in range(n+1, len(l)+1):
+                osa2.append(l([j]))
+            osa1.revers()
+            osa2.revers()
+            osa2.extend(osa1)
+            print(osa2)
+    except:
+        print("!!")
+        
+
+
+
+
+#1
+i=""# i-index
+m=["Tallinn", "Narva", "K-Järve", "Ida-Virumaa", "Tartu", "Tartumaa", "Viljandi", "Pärnumaa", "Saarema."]
+
+while True:
+    try:
+        i=int(input("Anna index: "))
+        if i<99999 and i>10000:
+            break
+    except:
+        print("Anna õgie index")
+i=i//10000 #1,2,3-9.
+print(f"{i} on {m[i-1]}")#0,1-8.
+if i in [1,2,3]:
+    print("Jätke kodu")
+else:
+    print("Kanna maske")
+
+#Самостоятельная работа
 spisok=[]
 l=["Kapibara", "Kot"]
 b=["Belka"]
